7dc69c000014edf3f2a93c960b5e5f3188928d28
Maria Vela <maria.vela790@gmail.com>
2018-10-03 12:14:08 -0500

    add functions-lecture.js practice exercise

--- js/external.js
+++ js/external.js
@@ -5,17 +5,26 @@ alert('Welcome to my Website!');
 var usersFavoriteColor = prompt('What is your favorite color?');
 alert('Great, ' + usersFavoriteColor + ' is my favorite color too!');
 
-
-//do problem number 3 using the prompt and alert functions!
-var littleMermaidCost = parseFloat(prompt('How much does the Little Mermaid cost to rent per day?'));
+var rentalRate = parseFloat(prompt('How much does it cost to rent per day?'));
 var littleMermaidRentTime = parseInt(prompt('How many days do you plan to rent this movie for?'));
-var brotherBearCost = parseFloat(prompt('How much does the brotherBearCost cost to rent per day?'));
 var brotherBearRentTime = parseInt(prompt('How many days do you plan to rent this movie for?'));
-var herculesCost = parseFloat(prompt('How much does the Hercules cost to rent per day?'));
 var herculesRentTime = parseInt(prompt('How many days do you plan to rent this movie for?'));
-var price = (littleMermaidCost * littleMermaidRentTime) + (brotherBearCost * brotherBearRentTime) + (herculesCost * herculesRentTime);
+var price = rentalRate * (littleMermaidRentTime + brotherBearRentTime + herculesRentTime);
 alert('The total cost for renting these movies is $' + price.toFixed(2));
 
+//do problem number 3 using the prompt and alert functions!
+//Movie rental problem: Solution takes into account that not all movies cost the same to rent.
+// var littleMermaidCost = parseFloat(prompt('How much does the Little Mermaid cost to rent per day?'));
+// var littleMermaidRentTime = parseInt(prompt('How many days do you plan to rent this movie for?'));
+// var brotherBearCost = parseFloat(prompt('How much does the brotherBearCost cost to rent per day?'));
+// var brotherBearRentTime = parseInt(prompt('How many days do you plan to rent this movie for?'));
+// var herculesCost = parseFloat(prompt('How much does the Hercules cost to rent per day?'));
+// var herculesRentTime = parseInt(prompt('How many days do you plan to rent this movie for?'));
+// var price = (littleMermaidCost * littleMermaidRentTime) + (brotherBearCost * brotherBearRentTime) + (herculesCost * herculesRentTime);
+// alert('The total cost for renting these movies is $' + price.toFixed(2));
+
+
+//Google pay problem
 var googlePayPerHour = parseFloat(prompt('How much does Google pay per hour?: '));
 var googleHoursWorked = parseFloat(prompt('How many hours did you work for Google this week?: '));
 var amazonPayPerHour = parseFloat(prompt('How much does Amazon pay per hour?: '));
@@ -32,35 +41,41 @@ totalPay += amazonHoursWorked * amazonPayPerHour;
 totalPay += facebookHoursWorked * facebookPayPerHour;
 alert('This week, you earned $' + totalPay.toFixed(2));
 
+//Student Enrollment problem
 var noConflict = confirm('Are you positive this class does not conflict? ');
 var classNotFull = confirm('Are you sure the class is not full? ');
 var canEnroll = (noConflict && classNotFull);
 alert('Then it is ' + canEnroll + ' that you can enroll.');
 
-var premiumMember = confirm('Do you have a premium membership? ');
-var moreThanTwoItems = confirm('Do you have more than two items? ');
-var discountApplied = (premiumMember || moreThanTwoItems);
+//Membership problem from point of view of store
+var numItems = 2;
+var offerValid = confirm('Is the offer still valid?');
+var premiumMember = confirm('Does customer have a premium membership? ');
+var numBoughtItems = prompt('Enter number of items customer wants to purchase: ');
+var enoughItems = numBoughtItems >= numItems;
+var discountApplied = (premiumMember || (enoughItems && offerValid));
 alert('Then it is ' + discountApplied + ' that you get the discount.');
 
-if(premiumMember || moreThanTwoItems){
-   alert('Great, discount is applied!');
-}
-else {
-    alert('Unfortunately we can\'t apply the discount');
-}
+// //Alternative solution to Membership problem using conditionals
+// if(premiumMember || (enoughItems && offerValid)){
+//    alert('Great, discount is applied!');
+// }
+// else {
+//     alert('Unfortunately we can\'t apply the discount');
+// }
 
-function checkDiscount(){
-    var premiumMember = confirm('Do you have a premium membership? ');
-    if(!premiumMember){
-        var moreThanTwoItems = confirm('Do you have more than two items? ');
-        if(moreThanTwoItems)
-            alert('Great, discount is applied!');
-        else
-            alert('Unfortunately we can\'t apply the discount because you need to be a member or have more than two items.');
-    }
-    else{
-        alert('Great, discount is applied!');
-    }
-}
-
-checkDiscount();
+// //Alternative solution to Membership problem using a function
+// function checkDiscount(){
+//     var premiumMember = confirm('Do you have a premium membership? ');
+//     if(!premiumMember){
+//         var moreThanTwoItems = confirm('Do you have more than ' + numItems + ' items? ');
+//         if(enoughItems && offerValid)
+//             alert('Great, discount is applied!');
+//         else
+//             alert('Unfortunately we can\'t apply the discount because you need to be a member or have more items.');
+//     }
+//     else{
+//         alert('Great, discount is applied!');
+//     }
+// }
+// checkDiscount();
---
+++ js/functions-lecture.js
@@ -0,0 +1,46 @@
+
+//function takes in string
+
+//get rid of white space
+
+//store count of the characters
+
+//return number for count of characters
+
+// function countLetters(input){
+//     //set default output to make sure something is always returned
+//     var output = 0;
+//     if()
+//     var strippedWhiteSpace = input.trim();
+//     output = strippedWhiteSpace.length;
+//     //do return statement first
+//     return output;
+// }
+
+//**make a function, hasLetterA() that takes in a string and returns true if the string contains "a"
+// and false if it does not; test the function
+
+//STEPS FOR hasLetterA()
+//take in a string
+
+//search for index of a
+
+//decide if index is valid
+
+//return whether or not index is valid
+
+function hasLetterA(strInput){
+    var output = false;
+    var strInput = strInput.toLowerCase();
+    output = strInput.indexOf('a') >= 0;
+    return output;
+}
+
+
+function hasLetterA(strInput){
+    var output = false;
+    output = (strInput.indexOf('a') >= 0) || (strInput.indexOf('A') >= 0);
+    return output;
+}
+
+
