efc4b59fb93bf49253280b47f7a5edf7083f8d5c
Maria Vela <maria.vela790@gmail.com>
2018-10-09 11:29:04 -0500

    beginning of snake game

---
+++ js/settings-js.js
@@ -0,0 +1,8 @@
+var myCanvas = document.getElementById('myCanvas');
+var ctx = myCanvas.getContext('2d');
+var snakeSize = 10;
+var width = 350;
+var height = 350;
+var score = 0;
+var snake;
+var food;
