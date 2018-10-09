bb48b3053e2ccd132dc9991776445015619d7c82
Maria Vela <maria.vela790@gmail.com>
2018-10-09 14:50:25 -0500

    continued extra work

---
+++ js/for_loops.js
@@ -0,0 +1,25 @@
+function showMultiplicationTable(number) {
+    for(var i = 1; i <= 10; i++){
+        console.log(number + ' x '
+        + i + ' = ' + (number * i));
+    }
+}
+showMultiplicationTable(7);
+
+for(var n = 1; n < 10; n++){
+    var num = parseInt(Math.random() * 180 + 20);
+    if(num % 2 === 0)
+        console.log(num + ' is even');
+    else
+        console.log(num + ' is odd');
+}
+
+for(var i = 1; i < 10; i++){
+    for(var j = 1; j <= i; j++){
+        console.log(i);
+    }
+}
+
+for(var i = 100; i >= 5; i -= 5){
+    console.log(i);
+}
