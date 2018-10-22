5dc25d7d76453590fc038a10ea89ffc57a19817e
Maria Vela <maria.vela790@gmail.com>
2018-10-22 18:00:46 -0500

    test

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,14 @@
+/**
+ *
+ Create a playable Tic-tac-toe board board game with unique styling and animations using jQuery.
+
+ 1. Clicks should alternate between the unicode symbol or images for X's and O's when clicking tiles.
+
+
+
+ 2. The current player's turn should be displayed after each click.
+
+ 3. Clicked tiles should no longer be clickable.
+
+ 4. A button should be included to start or restart a new game.
+ */
