b48648f4e40236e1342a6408c23230921eeeeb2f
Maria Vela <maria.vela790@gmail.com>
2018-11-01 09:53:48 -0500

    corrected solution

--- js/promises.js
+++ js/promises.js
@@ -1,39 +1,39 @@
-const wait = (number) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-
-            resolve('resolved');
-            // console.log('resolved');
-        }, number);
-    });
-};
-
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 second'));
-
-const checkLastCommit = (userName) => {
-    // let url = 'https://api.github.com/repos/' + userName + 'codeup-web-exercises/commits';
-    fetch('https://api.github.com/users/' + userName + '/events', {headers: {'Authorization' : ''}})
-        .then((response) => response.json())
-        .then((events) => {
-            console.log(events);
-            for(let event of events){
-                // (event) => {
-                if(event.type === 'PushEvent'){
-                    console.log(event.payload.commits[0]);
-                    console.log(event.created_at);
-                    return 0;
-                }
-                // };
-            }
+// const wait = (number) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//
+//             resolve('resolved');
+//             // console.log('resolved');
+//         }, number);
+//     });
+// };
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 second'));
 
 
 
-            // for (let event of events){
-            //     if(event.type === '')
-            // }
-        });
-};
+// const checkLastCommit = (userName) => {
+//     // let url = 'https://api.github.com/repos/' + userName + 'codeup-web-exercises/commits';
+//     fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization' : ''}})
+//         .then((response) => response.json())
+//         .then((events) => {
+//             // console.log(events);
+//             for(let event of events){
+//                 // (event) => {
+//                 if(event.type === 'PushEvent'){//looks for last push event project
+//                     fetch(`${event.repo.url}/commits`)//in that push event, we look at all the commits on that project
+//                         .then(response => response.json())
+//                         .then(commits => {
+//                             console.log(commits[0].commit.author.date);
+//                         });
+//                 }
+//                 break;
+//             }
+//         });
+// };
+//
+// checkLastCommit('mariaVela790');
 
-checkLastCommit('mariaVela790');
+
 
