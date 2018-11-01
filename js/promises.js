8bb64c58c032dc41761ced7c0f5d6e805a10146c
Maria Vela <maria.vela790@gmail.com>
2018-11-01 10:26:45 -0500

    added bonuses

--- js/promises.js
+++ js/promises.js
@@ -1,3 +1,4 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // const wait = (number) => {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
@@ -12,7 +13,7 @@
 // wait(3000).then(() => console.log('You\'ll see this after 3 second'));
 
 
-
+//////////////////////////////////////// GITHUB API ////////////////////////////////////////////////////////////////////
 // const checkLastCommit = (userName) => {
 //     // let url = 'https://api.github.com/repos/' + userName + 'codeup-web-exercises/commits';
 //     fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization' : ''}})
@@ -34,6 +35,62 @@
 // };
 //
 // checkLastCommit('mariaVela790');
+
+//////////////////////////////////// Bonuses ///////////////////////////////////////////////////////////////////////////
+// =================== BONUS exercise 1
+// In your data directory, create a profiles.json file with the following...
+// {
+//     "sandy123": "brown",
+//     "bob234": "green",
+//     "sally345": "blue"
+// }
+// Add an input field and submit button (you will need to prevent the default behaviour) that when clicked,
+// runs the following function...
+
+//Create a function that takes in a username and fetch's the color preference based on the username and
+// sets the background color to that color.
+
+
+
+
+// =================== BONUS exercise 2
+// Using the Star Wars API, log the homeworld of a searched character
+
+// =================== BONUS exercise 3
+// Using the GitHub API and reduce(), find the average hour of the last 3 pushes. Ignore minutes.
+
+// =================== BONUS exercise 4
+// Create star-chars.json file in your data folder and paste in the following...
+// [
+//     {
+//         "name": "Rey",
+//         "coolness": 9
+//     },
+//     {
+//         "name": "Luke Skywalker",
+//         "coolness": 9
+//     },
+//     {
+//         "name": "Darth Vadar",
+//         "coolness": 10
+//     }
+// ]
+// Create a function that fetches this array and returns an array of homeworlds for the characters.
+//     It will look something like the following...
+// ['Jakku', 'Tatooine', 'Tatooine']
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
