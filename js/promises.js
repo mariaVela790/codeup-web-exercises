6060ed8853beefffc744176fd2223675e727536e
Maria Vela <maria.vela790@gmail.com>
2018-10-31 19:04:02 -0500

    added exercises

--- js/promises.js
+++ js/promises.js
@@ -13,7 +13,7 @@ wait(3000).then(() => console.log('You\'ll see this after 3 second'));
 
 const checkLastCommit = (userName) => {
   // let url = 'https://api.github.com/repos/' + userName + 'codeup-web-exercises/commits';
-    fetch('https://api.github.com/users/' + userName + '/events', {headers: {'Authorization' : '71019bf4960aa50a452fadcdd2873f2bbaed16e0'}})
+    fetch('https://api.github.com/users/' + userName + '/events', {headers: {'Authorization' : ''}})
         .then((response) => response.json())
         .then((events) => {
             console.log(events);
