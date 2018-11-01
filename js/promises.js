d3af405e144d15ba433f77f454a5ecffca2fb009
Maria Vela <maria.vela790@gmail.com>
2018-10-31 19:10:26 -0500

    pushing thangs

--- js/promises.js
+++ js/promises.js
@@ -1,29 +1,29 @@
 const wait = (number) => {
-  return new Promise((resolve, reject) => {
-      setTimeout(() => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
 
-          resolve('resolved');
-          // console.log('resolved');
-      }, number);
-  });
+            resolve('resolved');
+            // console.log('resolved');
+        }, number);
+    });
 };
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 second'));
 
 const checkLastCommit = (userName) => {
-  // let url = 'https://api.github.com/repos/' + userName + 'codeup-web-exercises/commits';
+    // let url = 'https://api.github.com/repos/' + userName + 'codeup-web-exercises/commits';
     fetch('https://api.github.com/users/' + userName + '/events', {headers: {'Authorization' : ''}})
         .then((response) => response.json())
         .then((events) => {
             console.log(events);
             for(let event of events){
                 // (event) => {
-                    if(event.type === 'PushEvent'){
-                        console.log(event.payload.commits[0]);
-                        console.log(event.created_at);
-                        return 0;
-                    }
+                if(event.type === 'PushEvent'){
+                    console.log(event.payload.commits[0]);
+                    console.log(event.created_at);
+                    return 0;
+                }
                 // };
             }
 
